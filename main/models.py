0b04b024e7d67ded0731221b0dcbb2a736de9813
Aisyah Zayyana Hanifah <aiszayhan@gmail.com>
2026-09-14 16:19:21 +0700

    feat: Add Dynamic Education Section with MVT

--- main/models.py
+++ main/models.py
@@ -27,3 +27,19 @@ class Experience(models.Model):
     @property
     def is_ongoing(self):
         return self.ended_at is None
+
+class Education(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    institution = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255, blank=True)
+    description = models.TextField()
+    thumbnail = models.URLField(blank=True, null=True)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.institution
+
+    @property
+    def is_ongoing(self):
+        return self.ended_at is None
