58c39225b18cd8d543338478032b1434705aa8fb
Aisyah Zayyana Hanifah <aiszayhan@gmail.com>
2026-09-14 19:37:48 +0700

    feat: Add Unit Tests for Education Section

--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Education
 
 
 class MainTest(TestCase):
@@ -11,6 +11,7 @@ class MainTest(TestCase):
             title="Asisten Dosen PBP",
             description="Membantu mahasiswa memahami pengembangan web.",
             category="part-time",
+            started_at="2026-08-01",
         )
 
     def test_main_url_is_accessible(self):
@@ -39,7 +40,7 @@ class MainTest(TestCase):
         self.assertContains(response, self.experience.title)
         self.assertContains(response, self.experience.description)
         self.assertContains(response, "Part-Time")
-        self.assertContains(response, "Sedang berlangsung")
+        self.assertContains(response, "Currently ongoing")
         self.assertContains(response, f'href="{reverse("main:show_main")}"')
 
     def test_empty_experience_page(self):
@@ -54,5 +55,36 @@ class MainTest(TestCase):
         response = self.client.get(reverse("main:show_experience"))
 
         self.assertFalse(self.experience.is_ongoing)
-        self.assertContains(response, "Selesai")
-        self.assertNotContains(response, "Sedang berlangsung")
+        self.assertContains(response, "Completed")
+        self.assertNotContains(response, "Currently ongoing")
+
+    def test_education_page_is_accessible(self):
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "education.html")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+
+
+    def test_education_data_appears_on_page(self):
+        education = Education.objects.create(
+            institution="Universitas Indonesia, Faculty of Computer Science",
+            degree="S1 Sistem Informasi",
+            description="GPA: 5.00/4.00",
+            started_at="2025-08-01",
+            ended_at=None,
+        )
+
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, education.institution)
+        self.assertContains(response, education.degree)
+        self.assertContains(response, education.description)
+
+
+    def test_empty_education_page(self):
+        Education.objects.all().delete()
+
+        response = self.client.get(reverse("main:show_education"))
+
+        self.assertContains(response, "Belum ada pendidikan yang ditambahkan.")
