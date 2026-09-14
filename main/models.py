d2b304081366bb4cffdf99de37fe31ce1414a429
Aisyah Zayyana Hanifah <aiszayhan@gmail.com>
2026-09-14 13:18:04 +0700

    feat: Refine Experience Page

--- main/models.py
+++ main/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Experience(models.Model):
     EXPERIENCE_CHOICES = [
+        ('organization', 'Organizatiom'),
+        ('committee', 'Committee'),
         ('internship', 'Internship'),
         ('research', 'Research'),
         ('volunteer', 'Volunteer'),
@@ -13,10 +15,11 @@ class Experience(models.Model):
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
     thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.DateTimeField(auto_now_add=True)
+    started_at = models.DateTimeField()
     ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
